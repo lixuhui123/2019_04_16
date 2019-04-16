6f9dedd084a064bf6e4a1d1b99c82b399a6d367d
lixuhui-123 <2871726754@qq.com>
2019-04-16 23:06:39 +0800

    计算一个数二进制下有几个1

---
+++ 04_16/04_16/JiYiGeShuDeWei.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+//取一个数的二进制有几个1
+int BitOneCount(num)
+{
+	int count = 0;
+	for (int i = 0; i < 32; ++i)
+	{
+		if (num&(1 << i))
+			++count;
+	}
+	return count;
+	/*int count = 0;
+	while (num != 0)
+	{
+		if (num % 2 == 1)
+			count++;
+		num=num >> 1;
+	}
+	return count;*/
+}
+int main()
+{
+	int num = -10;
+	BitOneCount(num);
+	printf("%d\n", BitOneCount(num));
+	system("pause");
+	return 0;
+}
